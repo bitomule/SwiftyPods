8bc4a40400c8443a574cb2257c655dbf030aa7e1
David Collado <david.collado@wallapop.com>
2020-04-12 13:39:22 +0200

    Read stencil template

--- Package.swift
+++ Package.swift
@@ -9,14 +9,18 @@ let package = Package(
         .executable(name: "swiftypods", targets: ["SwiftyPods"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyPods",
-            dependencies: ["ArgumentParser"]),
+            dependencies: [
+                "ArgumentParser",
+                "Stencil"
+        ]),
         .testTarget(
             name: "SwiftyPodsTests",
             dependencies: ["SwiftyPods"]),
