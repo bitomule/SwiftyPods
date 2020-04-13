b0ff7c01e7b015459279239263d284ea3421aaa9
David Collado <david.collado@wallapop.com>
2020-04-13 21:05:17 +0200

    Add temporal path builder

--- Sources/PackageBuilder/PackageBuilder.swift
+++ Sources/PackageBuilder/PackageBuilder.swift
@@ -11,13 +11,18 @@ public final class PackageBuilder: PackageBuilding {
     // Files are set in method
     // Link files to folder using FileSymlinker
     private let templateLocator: TemplateLocating
+    private let temporalPathBuilder: TemporalPathBuilding
     
-    public init(templateLocator: TemplateLocating = TemplateLocator()) {
+    public init(
+        templateLocator: TemplateLocating = TemplateLocator(),
+        temporalPathBuilder: TemporalPathBuilding = TemporalPathBuilder()
+    ) {
         self.templateLocator = templateLocator
+        self.temporalPathBuilder = temporalPathBuilder
     }
     
     public func build(from path: URL) throws -> URL {
-        let temporalPath = URL(fileURLWithPath: "", isDirectory: true)
+        let temporalPath = try temporalPathBuilder.build(at: path)
         let files = try templateLocator.findTemplates(at: path)
         print(files)
         return URL(fileURLWithPath: "")
--- Sources/PackageBuilder/TemporalPathBuilder.swift
+++ Sources/PackageBuilder/TemporalPathBuilder.swift
@@ -1,1 +1,21 @@
 import Foundation
+
+public protocol TemporalPathBuilding {
+    func build(at path: URL) throws -> URL
+}
+
+public final class TemporalPathBuilder: TemporalPathBuilding {
+    private let manager: FileManager
+    
+    public init(manager: FileManager = FileManager.default) {
+        self.manager = manager
+    }
+    
+    public func build(at path: URL) throws -> URL {
+        let uuid = UUID().uuidString
+        let tmpPath = "tmp/" + uuid + "/"
+        let newPath = path.appendingPathComponent(tmpPath, isDirectory: true)
+        try FileManager.default.createDirectory(atPath: newPath.relativePath, withIntermediateDirectories: true, attributes: nil)
+        return newPath
+    }
+}
