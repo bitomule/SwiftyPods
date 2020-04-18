bf41adca3fa9e14541173073c3baddccb3af585c
David Collado <david.collado@wallapop.com>
2020-04-18 12:19:38 +0200

    Revert experiment

--- Sources/PackageBuilder/Templates/mainTemplate.swift
+++ Sources/PackageBuilder/Templates/mainTemplate.swift
@@ -1,7 +1,8 @@
-let mainTemplate = #"""
+let mainTemplate = """
 import Foundation
 import ArgumentParser
 import SwiftyPodsDSL
+import PodfileBuilder
 
 let podfiles = [{{podfiles}}]
 
@@ -16,87 +17,5 @@ struct Generate: ParsableCommand {
     }
 }
 
-// MARK: - Code required to generate template
-
-func buildPodfile(podfiles: [Podfile], path: String) throws {
-    // Check folder exists
-    let url = URL(fileURLWithPath: path, isDirectory: true)
-    let context = FixedContentBuilder().build(podfiles: podfiles)
-    let template = try FixedTemplateRenderer().render(
-        template: podfileTemplate,
-        context: context
-    )
-    try saveFile(name: "podfile", path: url, content: template)
-}
-
-private func saveFile(name: String, path: URL, content: String) throws {
-    let newFile = path.appendingPathComponent(name).path
-    FileManager.default.createFile(atPath: newFile, contents: content.data(using: .utf8), attributes: nil)
-}
-
-private final class FixedContentBuilder {
-    enum Constant {
-        static let propertyName = "pods"
-    }
-    
-    func build(podfiles: [Podfile]) -> [String: String] {
-        let lines = podfiles.map {
-            $0.targets.map {
-                $0.dependencies.map {
-                    $0.toString()
-                }.joined(separator: "\n")
-            }
-            .joined(separator: "\n")
-        }.joined(separator: "\n")
-        return [
-            Constant.propertyName: lines
-        ]
-    }
-}
-
-private final class FixedTemplateRenderer {
-    enum Constant {
-        static let fileName = "podfile"
-    }
-    
-    public func render(
-        template: String,
-        context: [String: String]
-    ) throws -> String {
-        context.reduce(template) { result, dict in
-            return generateFile(template: result, value: dict.value, keyToReplace: dict.key)
-        }
-    }
-    
-    public func render(
-        templateFile: URL,
-        context: [String: String]
-    ) throws -> String {
-        let template = try String(contentsOf: templateFile)
-        return context.reduce(template) { result, dict in
-            return generateFile(template: result, value: dict.value, keyToReplace: dict.key)
-        }
-    }
-    
-    private func generateFile(template: String, value: String, keyToReplace: String) -> String {
-        template.replacingOccurrences(of: "{{\(keyToReplace)}}", with: value)
-    }
-    
-    private func filePath(from url: URL) -> String {
-        url.deletingLastPathComponent().relativeString
-    }
-    
-    private func fileName(from url: URL) -> String {
-        url.lastPathComponent
-    }
-}
-
-let podfileTemplate = """
-platform :ios, '13.0'
-inhibit_all_warnings!
-
-{{pods}}
-"""
-
 Generate.main()
-"""#
+"""
--- Sources/PodfileBuilder/PodfileBuilder.swift
+++ Sources/PodfileBuilder/PodfileBuilder.swift
@@ -1,75 +1,15 @@
 import Foundation
+import Storage
 import SwiftyPodsDSL
+import TemplateRenderer
 
 public func buildPodfile(podfiles: [Podfile], path: String) throws {
     // Check folder exists
     let url = URL(fileURLWithPath: path, isDirectory: true)
-    let context = FixedContentBuilder().build(podfiles: podfiles)
-    let template = try FixedTemplateRenderer().render(
+    let context = ContentBuilder().build(podfiles: podfiles)
+    let template = try TemplateRenderer().render(
         template: podfileTemplate,
         context: context
     )
-    try saveFile(name: "podfile", path: url, content: template)
-}
-
-private func saveFile(name: String, path: URL, content: String) throws {
-    let newFile = path.appendingPathComponent(name).path
-    FileManager.default.createFile(atPath: newFile, contents: content.data(using: .utf8), attributes: nil)
-}
-
-private final class FixedContentBuilder {
-    enum Constant {
-        static let propertyName = "pods"
-    }
-    
-    func build(podfiles: [Podfile]) -> [String: String] {
-        let lines = podfiles.map {
-            $0.targets.map {
-                $0.dependencies.map {
-                    $0.toString()
-                }.joined(separator: "\n")
-            }
-            .joined(separator: "\n")
-        }.joined(separator: "\n")
-        return [
-            Constant.propertyName: lines
-        ]
-    }
-}
-
-private final class FixedTemplateRenderer {
-    enum Constant {
-        static let fileName = "podfile"
-    }
-    
-    public func render(
-        template: String,
-        context: [String: String]
-    ) throws -> String {
-        context.reduce(template) { result, dict in
-            return generateFile(template: result, value: dict.value, keyToReplace: dict.key)
-        }
-    }
-    
-    public func render(
-        templateFile: URL,
-        context: [String: String]
-    ) throws -> String {
-        let template = try String(contentsOf: templateFile)
-        return context.reduce(template) { result, dict in
-            return generateFile(template: result, value: dict.value, keyToReplace: dict.key)
-        }
-    }
-    
-    private func generateFile(template: String, value: String, keyToReplace: String) -> String {
-        template.replacingOccurrences(of: "{{\(keyToReplace)}}", with: value)
-    }
-    
-    private func filePath(from url: URL) -> String {
-        url.deletingLastPathComponent().relativeString
-    }
-    
-    private func fileName(from url: URL) -> String {
-        url.lastPathComponent
-    }
+    try FileSystem().saveFile(name: "podfile", path: url, content: template, overwrite: true)
 }
